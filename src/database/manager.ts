4c56fcd75133b1d70cdad9457a840c15c4d878ba
Appraisily <134934621+Appraisily@users.noreply.github.com>
2025-01-23 14:40:57 +0100

    aaaa

--- src/database/manager.ts
+++ src/database/manager.ts
@@ -1,10 +1,12 @@
-import { Pool } from 'pg';
+import pg from 'pg';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 import logger from '../utils/logger.js';
 import type { initConfig } from '../config/index.js';
 import dns from 'dns';
 import { promisify } from 'util';
+
+const { Pool } = pg;
 
 const lookup = promisify(dns.lookup);
 
